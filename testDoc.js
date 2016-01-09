8f26de569d1ffb37d59db3f4e661e8471cafa7ce
clif edwards <clif.edwards.2@gmail.com>
2016-01-09 16:57:40 -0500

    ADDED 2nd test: SELECT_vGrpStyleObj: a function TO SELECT A verse Group's _vGrpStyleObj

--- testDoc.js
+++ testDoc.js
@@ -16,3 +16,15 @@ console.assert(___srt,`___cut = GET_VerseGrpsArr ();
 ___srt = (___cut.length === 3 &&
     isArray(___cut)
 `);
+
+/**
+ * SELECT_vGrpStyleObj: a function TO SELECT A verse Group's _vGrpStyleObj
+ */
+___data = GET_VerseGrpsArr()[2];  //TEST DATA >> v Grp div.fut
+// BUILD away
+___ret = SELECT_vGrpStyleObj(___data)(StyleObj);
+___msg = "CONFIRM this is THE futStyleObj.";
+___srt = ___ret.name === 'fut' && ___ret.smlWt === 0.5;
+console.assert(___srt, ___msg); // true
+// OK, I can EXTRACT the style settings FOR A verseGrp FROM global StyleObj
+//*****************************************************
