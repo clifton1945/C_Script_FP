b4bb5451ef54d3b343537bc49ae96b1f9f3d0fa3
clif edwards <clif.edwards.2@gmail.com>
2016-01-15 10:58:48 -0500

    1TAT: SPLIT functions INTO 2 files. >>
     h_SET_All_verse_Styles.js  NOW HAS all it's own functions USED IN testDocs.js
    
    SOON:  IN BindHandlers.js
     (1)  call it somehow
     (2) point Handlers / CallBacks to tomethinbg useful.

--- src/functions.js
+++ src/functions.js
@@ -59,51 +59,11 @@ function pipeline() {
 //C_It(addTwoThenTimesFive(1)); //=> 15
 //addTwoThenTimesFive(-1); //=> 5
 //C_It(addTwoThenTimesFive(100)); //=> 510
-
-const GET_cur_crGrps_Ary = pipeline(
-    ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-    //,C_Trace((str) => `query str:${str}`)
-    ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-    ,Coll2Array
-    //,C_TraceD((ary) => `GET_cur_crGrps_Ary>> ${ary.length} VerseReadGrps`)
-);  // fn ()  returns a value, if CALLEDBY ()
-//let ___cut = GET_cur_crGrps_Ary; // NOTE: () >> value
-//let ___msg = "should look like an array with div.pst...cur...fut";
-//let ___srt = ___cut.length === 3 && isArray(___cut);
-//console.assert(___srt, ___msg);
-
-const SET_One_verseGrp_Styles = (styleObj) => (vrGrp) => {
-    let sO = styleObj[vrGrp.className];
-    //BUILD callback fn: SET_Verse_Style for use in map(fn)(vrGrp.chkldren)
-    ___cut = SET_One_Verse_Style(sO);           //REFACT use directly in f_map
-    ___data = Coll2Array(vrGrp.children);       //REFACT use directly in f_map
-    //C_It(`_this_StyleObj:${sO.name}`);
-    //C_It(`_this_VerseReadGrp.len:${___data.length}`);
-    f_map( ___cut ) ( ___data   );
-};  // CALLEDBY ( global StyleObj)(VerseGrp) >> just the StyleObj data for this VersereadGroup
-
-const SET_All_verse_Styles = (globalStyleObj) =>  (data) => {
-    //C_TraceD()(globalStyleObj);
-    //C_Trace((f)=>`fn;${f}`)(data);
-    f_map(SET_One_verseGrp_Styles (globalStyleObj))( data); // calls each of 3 VerseReadGrps
-};
-/**
- * GET_VerseReadGrpsArr:  use hardcoded query RETURNS current chapter's 3 verseGroups
- *    AS an array.
- *    hard code because I ALWAYS want just the currrent Chapter.
- */
-const GET_VerseReadGrpsArr = pipeline(
-        ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-        //,C_Trace((str) => `query str:${str}`)
-        ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-        ,Coll2Array
-        //,C_Trace((ary) => `${ary.length} VerseReadGrps`)
-);  // fn ()  returns a value, if CALLEDBY ()
-
 ///**
 // * calculates a specific verse style wt USING StyleObj.calcWt
 // * @param so: StyleObj
 // */
+
 //const calcWt = (so) => (vo) => {
 //    return so.calcWt(so, vo); // return a style wt.
 //};
--- tst/testDoc.js
+++ tst/testDoc.js
@@ -3,19 +3,6 @@
 
 let ___cut, ___srt, ___ret, ___msg, ___pipe, ___data;
 // CURRENT WIP  WWWWWWWWWWWWWWWWWWWWWWWW IIIIIIIIIIIIIIIIIIIIIIIIIIII PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
-///**
-// * UPDATES  the global VerseObj FOR this vers elem.
-// * @param vO:  the global VerseObj
-// * @constructor
-// */
-//const UPDATE_VerseObject = (vO) => (vers) => {
-//    vO.val = vers[0];
-//    vO.ndx = vers[1];
-//    vO.ary = vers[2];
-//    //C_Trace((o) => o.ndx)(vO);
-//    return vO
-//};
-//// SET_One_Verse_Style:: ( obj, HTML:div) => some return signal complete
 ////************************************************.
 //// CUT: CodeUnderTest ****************************
 const SET_One_Verse_Style = (sO) => (...verse) => {
@@ -30,7 +17,7 @@ const SET_One_Verse_Style = (sO) => (...verse) => {
     C_Both(vO.toStr())
 };
 //*****************************************************
-// RUN
+// RUNSPLIT functions INTO 2 files.
 SET_All_verse_Styles(StyleObj)(GET_cur_crGrps_Ary ()); // fn () <== INVOKED W/ ()
 //*****************************************************
 /**
