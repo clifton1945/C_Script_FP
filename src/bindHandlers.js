d13afd3e999c813c802ae1860343e8b56ac96d20
clif edwards <clif.edwards.2@gmail.com>
2016-01-15 08:09:38 -0500

    ADDED copy OF BindHandlers.js FROM CeeVerses_KISS.Job.html

---
+++ src/bindHandlers.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var BindHandlers = function BindHandlers(book) {
+    //document.addEventListener("keydown", keysPressed, false);
+    document.addEventListener("keyup", keyActions, false);
+    function keyActions(e) {
+        // read Last Chptr.
+        if (e.keyCode == 37) {
+            //e.stopPropagation();
+            e.preventDefault();
+            consIt("read Last Chptr");
+            book.read_lstChptr();
+        }
+        // read Last verse.
+        if (e.keyCode == 38) {
+            //e.stopPropagation();
+            e.preventDefault();
+            consIt("read Last Verse");
+            book.read_lstVerse();
+
+        }
+        // read Next Chptr.
+        if (e.keyCode == 39 || e.keyCode == 96) { // rt arrow || numpad 0
+            //e.stopPropagation();
+            e.preventDefault();
+            consIt("read Next Chptr ");
+            book.read_nxtChptr();
+        }
+        // read Next verse.
+        if (e.keyCode == 32 || e.keyCode == 40) {
+            //e.stopPropagation();
+            e.preventDefault();
+            consIt("read Next Verse");
+            book.read_nxtVerse();
+        }
+    }
+};
