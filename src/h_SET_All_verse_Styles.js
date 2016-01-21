7b4ef242818e4fe86faf5c6945c1959d86b25ddf
clif edwards <clif.edwards.2@gmail.com>
2016-01-21 13:35:16 -0500

    REFACT commented out Unused  h_SET....js code 1TAT: 160121  UPDATE style AFTER A CALC TO UPDATE_ReadGrps

--- src/h_SET_All_verse_Styles.js
+++ src/h_SET_All_verse_Styles.js
@@ -4,8 +4,6 @@
  */
 "use strict";
 
-//const Coll2Array = (coll) => [...coll];
-
 /**
  * UPDATES  the global VerseObj FOR this vers elem.
  * @param vO:  the global VerseObj
@@ -31,30 +29,30 @@ const SET_One_Verse_Style = (sO) => (...verse) => {
     //C_Both(vO.toStr())
 };
 
-const GET_cur_crGrps_Ary = pipeline(
-    ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-    //,C_Trace((str) => `query str:${str}`)
-    ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-    ,Coll2Array
-    //,C_TraceD((ary) => `GET_cur_crGrps_Ary>> ${ary.length} VerseReadGrps`)
-);  // fn ()  returns a value, if CALLEDBY ()
-//let ___cut = GET_cur_crGrps_Ary; // NOTE: () >> value
-//let ___msg = "should look like an array with div.pst...cur...fut";
-//let ___srt = ___cut.length === 3 && isArray(___cut);
-//console.assert(___srt, ___msg);
+//const GET_cur_crGrps_Ary = pipeline(
+//    ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
+//    //,C_Trace((str) => `query str:${str}`)
+//    ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
+//    ,Coll2Array
+//    //,C_TraceD((ary) => `GET_cur_crGrps_Ary>> ${ary.length} VerseReadGrps`)
+//);  // fn ()  returns a value, if CALLEDBY ()
+////let ___cut = GET_cur_crGrps_Ary; // NOTE: () >> value
+////let ___msg = "should look like an array with div.pst...cur...fut";
+////let ___srt = ___cut.length === 3 && isArray(___cut);
+////console.assert(___srt, ___msg);
 
-/**
- * GET_VerseReadGrpsArr:  use hardcoded query RETURNS current chapter's 3 verseGroups
- *    AS an array.
- *    hard code because I ALWAYS want just the currrent Chapter.
- */
-const GET_VerseReadGrpsArr = pipeline(
-        ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-        //,C_Trace((str) => `query str:${str}`)
-        ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-        ,Coll2Array
-        //,C_Trace((ary) => `${ary.length} VerseReadGrps`)
-);  // fn ()  returns a value, if CALLEDBY ()
+///**
+// * GET_VerseReadGrpsArr:  use hardcoded query RETURNS current chapter's 3 verseGroups
+// *    AS an array.
+// *    hard code because I ALWAYS want just the currrent Chapter.
+// */
+//const GET_VerseReadGrpsArr = pipeline(
+//        ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
+//        //,C_Trace((str) => `query str:${str}`)
+//        ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
+//        ,Coll2Array
+//        //,C_Trace((ary) => `${ary.length} VerseReadGrps`)
+//);  // fn ()  returns a value, if CALLEDBY ()
 const SET_One_verseGrp_Styles = (styleObj) => (vrGrp) => {
     let sO = styleObj[vrGrp.className];
     //BUILD callback fn: SET_Verse_Style for use in map(fn)(vrGrp.chkldren)
