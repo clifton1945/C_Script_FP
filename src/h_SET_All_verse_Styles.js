b5dbf1fad7f6655f9902e4fa3efcb002380a86bb
clif edwards <clif.edwards.2@gmail.com>
2016-01-21 19:15:51 -0500

    WHY NO SEE Traces in SET_ALL_Verse_Styles???

--- src/h_SET_All_verse_Styles.js
+++ src/h_SET_All_verse_Styles.js
@@ -16,7 +16,6 @@ const UPDATE_VerseObject = (vO) => (vers) => {
     //C_Trace((o) => o.ndx)(vO);
     return vO
 };
-
 const SET_One_Verse_Style = (sO) => (...verse) => {
     let vO = UPDATE_VerseObject (VerseObj) (verse) ;
     // so now HAVE both styleObj && verseObj
@@ -28,31 +27,6 @@ const SET_One_Verse_Style = (sO) => (...verse) => {
     //v_style.textAlign = 'center';
     //C_Both(vO.toStr())
 };
-
-//const GET_cur_crGrps_Ary = pipeline(
-//    ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-//    //,C_Trace((str) => `query str:${str}`)
-//    ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-//    ,Coll2Array
-//    //,C_TraceD((ary) => `GET_cur_crGrps_Ary>> ${ary.length} VerseReadGrps`)
-//);  // fn ()  returns a value, if CALLEDBY ()
-////let ___cut = GET_cur_crGrps_Ary; // NOTE: () >> value
-////let ___msg = "should look like an array with div.pst...cur...fut";
-////let ___srt = ___cut.length === 3 && isArray(___cut);
-////console.assert(___srt, ___msg);
-
-///**
-// * GET_VerseReadGrpsArr:  use hardcoded query RETURNS current chapter's 3 verseGroups
-// *    AS an array.
-// *    hard code because I ALWAYS want just the currrent Chapter.
-// */
-//const GET_VerseReadGrpsArr = pipeline(
-//        ( str = '#cur_ChptrReadGrp  .VerseReadGrps > div') => str  // > str
-//        //,C_Trace((str) => `query str:${str}`)
-//        ,(str) => document.querySelectorAll(str) // > NodeList w/ 3 divs.
-//        ,Coll2Array
-//        //,C_Trace((ary) => `${ary.length} VerseReadGrps`)
-//);  // fn ()  returns a value, if CALLEDBY ()
 const SET_One_verseGrp_Styles = (styleObj) => (vrGrp) => {
     let sO = styleObj[vrGrp.className];
     //BUILD callback fn: SET_Verse_Style for use in map(fn)(vrGrp.chkldren)
@@ -62,11 +36,18 @@ const SET_One_verseGrp_Styles = (styleObj) => (vrGrp) => {
     //C_It(`_this_VerseReadGrp.len:${___data.length}`);
     f_map( ___cut ) ( ___data   );
 };  // CALLEDBY ( global StyleObj)(VerseGrp) >> just the StyleObj data for this VersereadGroup
-
 const SET_All_verse_Styles = (globalStyleObj) =>  (curVerseGrp) => {
     C_TraceD()(globalStyleObj);
     C_Trace((f)=>`fn:${f}`)(curVerseGrp);
+    var curChptr_CRGrps = query(
+        '.ChptrReadGrps > .cur')(book);
+    Trace(()=>`CRGrps:${curChptr_CRGrps}`);
+    var curChptr_VRGrps = query(
+        ' .chptr > .VerseReadGrps > .cur')(curChptr_CRGrps);
+    Trace(()=>`VRGrps:${curChptr_VRGrps}`);
+
+
     // calls each of 3 VerseReadGrps
-    f_map(SET_One_verseGrp_Styles (globalStyleObj))( curVerseGrp);
+    f_map(SET_One_verseGrp_Styles (globalStyleObj))( curVerseGrp); // TODO  KEEP OR DROP
 
 };
