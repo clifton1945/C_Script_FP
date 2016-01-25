3271120880293dbb52d5d209b36c3fa729f3f01f
clif edwards <clif.edwards.2@gmail.com>
2016-01-25 15:26:11 -0500

    WIP ON testDocs.js  to BUILD a READ_next()  function  ....1TAT:: BUILD a functional  *   READ_Next(eg: last_curChild >> first_fut_Child : APPEND_Child(toGrp)(frGrp)  *   READ_Last(eg; first_first_curC >> lst_pstC: INSERT_Before(frGrp)(toGrp)(
     1TAT: EXPLORE See individual WORDS
    .....1TAT: NEXT  HiLITE SELECTED WORDS
    .....1TAT: NEXT  INPUT Regex AND HiLite

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -6,9 +6,11 @@ let A_Cut,  ___cut, ___srt, A_Ret, ___msg, ___pipe, ___data;
  *HOW MAKE && TEST functional UPDATE_ReadGrps(cur_ReadGrp)(direction)
  *   1TAT:  REPLACE UPDATE_ReadGrps(cur_ReadGrp)(direction)
  *      WITH more Functional Style
- *   REQUIRED BASIC FUNCTIONS:
+ *   // REQUIRED BASIC FUNCTIONS:
  *   const = GET_ = (Prop)(OF_El)=>el[prop]  //in this case Element Child
  *   const = IS_ = (Test) => GET_ (Prop) (OF_El)=>
+ *   // READ_Next || READ_Last functions
+ *   const Pst_
  *   READ_Next(eg: last_curChild >> first_fut_Child : APPEND_Child(toGrp)(frGrp)
  *   READ_Last(eg; first_first_curC >> lst_pstC: INSERT_Before(frGrp)(toGrp)(
  *   becomes
@@ -33,37 +35,24 @@ const book = query('.book')(document);
 const ChptrReadGrps = query('.ChptrReadGrps')(document);  //> div.ChptrReadGrps
 const ChptrReadGrpsChildren = query('.ChptrReadGrps')(document).children;  //> HTML Collection [3]
 const Cur_ChptrReadGrp = query('.ChptrReadGrps > .cur')(document); //> div.cur
-// CUT:  GET_(property)(OF_Elem)
-A_Ret = GET_('childElementCount')(Cur_ChptrReadGrp);
-A_Cut =IS_((o)=> o.childElementCount > 0)(Cur_ChptrReadGrp);
-Trace((o)=>`${A_Cut}`)(A_Cut);
-console.assert(A_Cut,`EXP: Cur_ChptrReadGrp Count>0 [${A_Cut}]`);
+//TESTS
+//A_Ret = GET_('childElementCount')(Cur_ChptrReadGrp);  //>> OK
+//A_Cut =IS_((o)=> o.childElementCount > 0)(Cur_ChptrReadGrp);
+////Trace((o)=>`${A_Cut}`)(A_Cut);
+//console.assert(A_Cut,`EXP: Cur_ChptrReadGrp Count>0 [${A_Cut}]`);
+// CUT:  BUILD AND TEST READ_Next()
+let curGrp = Cur_ChptrReadGrp();
+let pstGrp = GET_('previousElementSibling')(curGrp);
+let futGrp = GET_('nextElementSibling')(curGrp);
+IS_Next = IS_(((o)=>(o.childElementCount > 0)(futGrp)
+let READ_Next = pipeline (
+    (futGrp)=>(curGrp, pstGr) => {
+
+
+
+    }
+};
+
+
 //  POSTPONE THIS CALL SET_All_Verse_Styles (StyleObj);
-//BindHandlers(book);7
-
-/**
- *HOW TEST functional ??
- */
-
-/**
- * THINKING
- * I'll only want to CEE the cur_ChapterReadGrp AND UPDATE Style of the VerseReadGrps.
- * THEREFORE the default querySelectorAll string WILL BE( '#cur_ChptrReadGrp  .VerseReadGrps > div')
- * I'll css display: none everything else.
- * SO
- * I'll constantly BE READING: next || last verse // chptr
- *  SO how about a main() WITH
- *      GET_cur_crGrps_Ary(str) // =>vrGrps:[o,o,o]     // data:: CURRENT ChpterGrp [w/3 vrGrps:[w/n vers]]
- *      MAP(SET_All_versesStyle)(vrGrps)            //    each vrGrp:: vrGrp:[w/n vers]] IS
- *          SET_All_versesStyle(vrGrp)              //    APPLIED TO this fn => all verses.style updated
- *              SELECT_vStyleObj(StyleObj)(vrGrp)     //=>vStyleObj::sObj
- *              CALC_StyleWt_WITH(vStyleObj)                 //=>  partial curry CALC...::fn
- *              GET_versAry_FROM(vrGrp)                       //=> data: versAry w/ n verses.
- *          MAP(SET_1verseStyle)(versAry)                // each verse of this vrGrp's versAry IS
- *              SET_1verseStyle( versAry)                 // APPLIED TO this fn
- *                  TRNFRM_TO_VersObj_FRM(VerseObj)(versAry)           //=> versObj
- *                  CALC_StyleWt_WITH(~vStyleObj~)(VersObj)     // => styleWt NOTE ~curried~
- *                  SET_Verse_Style_Str(styleWt)            //=> styleStr
- *                  SET_Verse_Style( styleStr)              //=> actual vers setAttribute
- *
- */
+//BindHandlers(book);
