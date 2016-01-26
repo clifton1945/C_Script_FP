14a1a36a102ccf575804d6dcf449f6d7af023d7b
clif edwards <clif.edwards.2@gmail.com>
2016-01-26 13:37:51 -0500

    GOT_ItWorking:  CREATE && TEST: MOVE_Next()
     ....1TAT:: BUILD a functional UPDATE_ReadGrps()
    
     1TAT: EXPLORE See individual WORDS
    .....1TAT: NEXT  HiLITE SELECTED WORDS
    .....1TAT: NEXT  INPUT Regex AND HiLite

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -24,26 +24,12 @@ let A_Cut,  ___cut, ___srt, A_Ret, ___msg, ___pipe, ___data;
 // TESTS
 //I WILL USE chapters, instead of  Verses, FOR these test.
 //  EXPECT three div classes: pat, cur, fut
-const book = query('.book')(document);
-const ChptrReadGrps = query('.ChptrReadGrps')(document);  //> div.ChptrReadGrps
-const ChptrReadGrpsChildren = query('.ChptrReadGrps')(document).children;  //> HTML Collection [3]
+//const book = query('.book')(document);
+//const ChptrReadGrps = query('.ChptrReadGrps')(document);  //> div.ChptrReadGrps
+//const ChptrReadGrpsChildren = query('.ChptrReadGrps')(document).children;  //> HTML Collection [3]
 const Cur_ChptrReadGrp = query('.ChptrReadGrps > .cur')(document); //> div.cur
-//TESTS
-//A_Ret = GET_('childElementCount')(Cur_ChptrReadGrp);  //>> OK
-//A_Cut =IS_((o)=> o.childElementCount > 0)(Cur_ChptrReadGrp);
-////Trace((o)=>`${A_Cut}`)(A_Cut);
-//console.assert(A_Cut,`EXP: Cur_ChptrReadGrp Count>0 [${A_Cut}]`);
-//
-// CUT:  BUILD AND TEST:
-//  GET_Grps(curGrpTmpl) //> GrpsObj
-//  SELECT_(selector)(Direction)=> 1 of 2 READ_(Next || Last)
-//  UPDATE_ReadGrps(GrpsObj) => {
-//      lets USE READ_Next() as example code
-//      ALLOW = fut.childElementCount >0
-//      MOVE fut>>cur & cur>>pst
-//}
 
-//  TEST: GET_Grps(curGrpTmpl)
+//  BUILD: GET_Grps(curGrpTmpl)
 /**
  *
  * @param tmplStr
@@ -56,31 +42,74 @@ const GET_Grps = (tmplStr) => { // > Obj{pst: po, cur: co, fut:fo}
     let fut = GET_('nextElementSibling')(cur);
     return {pst, cur, fut};
 };
+//  TESTS GET_Grps
 let curGrpTmpl = '.ChptrReadGrps > .cur';
 let GrpsObj = GET_Grps(curGrpTmpl);
+
 // REMEMBER These ARE Chapters!! NOT Verses
 let {pst, cur, fut} = GrpsObj;
 //Trace((o)=>`GET_Grps:${Object.keys(o)}`)(GrpsObj);
 console.assert(Object.keys(GrpsObj).length === 3
     , 'EXP 3 keys in GrpsObj.');  //> OK
 //
-// BUILD / TEST: READ_NextChild (toGrp)(frmGrp)
-const READ_NextChild = (frmGrp)=>(toGrp)=>{
+//
+// BUILD / TEST: MOVE_NextChild (toGrp)(frmGrp)
+
+// BUILD MOVE_NextChild()
+const MOVE_NextChild = (frmGrp)=>(toGrp)=>{
     toGrp.appendChild(frmGrp.firstElementChild);
 };
-//const READ_LastChild
-let n0=cur.childElementCount;
-Trace((o)=>`cur Chptr El Count:${n0}`)(n0);
-A_Cut = READ_NextChild(fut)(cur);
-let n1=cur.childElementCount;
-Trace((o)=>`cur Chptr El Count:${n1}`)(n1);
-console.assert(n1 === (n0+1)
-    , 'EXP ADDED fut TO cur');  //> OK
+////TEST MOVE_NextChild()
+//let n0=cur.childElementCount;
+//Trace((o)=>`cur Chptr El Count:${n0}`)(n0);
+//A_Cut = MOVE_NextChild(fut)(cur);
+//
+//let n1=cur.childElementCount;
+//Trace((o)=>`cur Chptr El Count:${n1}`)(n1);
+//console.assert(n1 === (n0+1)
+//    , 'EXP MOVED fut>>cur');  //> OK
+//
+//A_Cut = MOVE_NextChild(cur)(pst);
+//let n2=cur.childElementCount;
+//Trace((o)=>`cur Chptr El Count:${n2}`)(n2);
+//console.assert(n2 === n0
+//    , 'EXP MOVED cur>>pst'
+//);  //> OK
 
-
-// BUILD / TEST: READ_Next( GrpsObj)=> UPDATES DOM div.ReadGrps contents
-const READ_Next = (GrpsObj)=>{
+// BUILD MOVE_Next( GrpsObj)=> UPDATES DOM div.ReadGrps contents
+const MOVE_Next = (GrpsObj)=>{
     // will need CanMOVE, MOVE fut>>cur, MOVE cur>>pst
+    let {pst, cur, fut} = GrpsObj;
+    if( fut.childElementCount > 0 ) {
+            MOVE_NextChild(fut)(cur);
+            MOVE_NextChild(cur)(pst);
+    }
+};
+//TEST MOVE_Next()
+let n1=pst.childElementCount;
+let n2=cur.childElementCount;
+let n3=fut.childElementCount;
+Trace((o)=>`Before Move_Next():${n1} pst, ${n2} cur, ${n3} fut`)();
+
+A_Cut = MOVE_Next(GrpsObj);
+let n4=pst.childElementCount;
+let n5=cur.childElementCount;
+let n6=fut.childElementCount;
+Trace((o)=>`   1st Move_Next():${n4} pst, ${n5} cur, ${n6} fut`)();
+console.assert(n4+n5+n6 === 3
+    , '  1st MOVE_Next() EXP cur>>pst, fut>>cur'
+);  //> OK
+A_Cut = MOVE_Next(GrpsObj);
+let n7=pst.childElementCount;
+let n8=cur.childElementCount;
+let n9=fut.childElementCount;
+Trace((o)=>`   2nd MOVE_Next():${n4} pst, ${n5} cur, ${n6} fut`)();
+console.assert(n4===n7 && n5===n8 && n6===n9,
+    '  2nd MOVE_Next() EXP: No Change since fut.count===0 '
+);  //> OK
+// BUILD && TEST UPDATE_ReadGrps(Direction)
+const UPDATE_ReadGrps = (Direction)=>{
+
 };
 
 //  POSTPONE THIS CALL SET_All_Verse_Styles (StyleObj);
