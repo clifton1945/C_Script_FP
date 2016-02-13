816807f48942f293cb0b82c7669f5e324097f5ef
clif edwards <clif.edwards.2@gmail.com>
2016-02-13 17:56:26 -0500

    READY TO BEGIN: FP OF NOTE: MUCH WORK STILL TO BE DONE - BUT IT'S A START WORKING && STABLE || SEMI TESTS: MOVE_Next() and MOVE_Last() 1TAAT:

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -4,23 +4,16 @@ let C_Cut, C_Ret, C_Msg;
 // CUT: CodeUnderTest ****************************
 const book = document.querySelector('.book');
 const C_Grp_Tmpl = '.ChptrReadGrps > div';
-const V_Grp_Tmpl = '.VerseReadGrps > div';
-// the two _Grp_NL WILL NEED TO BE REQUERIED EACH ReadEvent.
+const V_Grp_Tmpl = '.ChptrReadGrps > .cur .VerseReadGrps > div';
+const C_Grp_NL = book.querySelectorAll(C_Grp_Tmpl); // NL:: 3 div.classes: pst, cur, fut
+const V_Grp_NL = book.querySelectorAll(V_Grp_Tmpl); // NL:: 3 div.classes: pst, cur, fut
+
+// TODO  WILL the two _Grp_NL NEED TO BE REQUERIED EACH ReadEvent ??????
 // AND PASSED TO the appropriate READ_Last() || READ_First()
 // CodeUnderTest: NodeListObject
-const C_Grp_NL = book.querySelectorAll(C_Grp_Tmpl); // NL:: 3 div.classes: pst, cur, fut
-const PST = Symbol();
-const CUR = Symbol();
-const FUT = Symbol();
-C_Grp_NL[PST] = C_Grp_NL[0];
-C_Grp_NL[CUR] = C_Grp_NL[1];
-C_Grp_NL[FUT] = C_Grp_NL[2];
-// FUNCTIONS
-
-const GET_childCount = (SYM) => R.prop('childElementCount',C_Grp_NL[SYM]);
-const GET_firstChild = (SYM) => R.prop('firstElementChild',C_Grp_NL[SYM]);
-const GET_lastChild = (SYM) => R.prop('lastElementChild',C_Grp_NL[SYM]);
-
+const PST = 0;
+const CUR = 1;
+const FUT = 2;
 /**
  * TESTS READ_...(querySelectorAll_Tmpl) -> ModifiedDOM {
     transform:: str -> NLO;
@@ -30,7 +23,7 @@ const GET_lastChild = (SYM) => R.prop('lastElementChild',C_Grp_NL[SYM]);
         READ_1_Grp (to: pst, frm: cur);
     }
  */
-C_Msg = "tst:READ_ Grp Next && Last:\n";
+C_Msg = "tst:Grp NodeList \n";
 //TESTS GET_Grp_NLO:: Str -> NodeList obj
 const GET_className = (SYM) => R.prop('className',C_Grp_NL[SYM]);
 C_Msg += ` EXP:pst===${GET_className(PST)}, \n`;
@@ -40,7 +33,43 @@ C_Both(C_Msg);
 // WRAP UP TESTS: GET_Grp_NLO
 let tst = GET_className(PST)==='pst' && GET_className(CUR)==='cur' && GET_className(FUT)==='fut';
 console.assert(tst);
-// TESTS: READ_Next
+//
+// Functions
+// TODO ADD TEST: CAN_READ_Next || CAN_READ_Last
+//const CAN_MOVE = R.propSatisfies(GET_childCount(frmSYM);  // maybe use R.isEmpty:: a->Bool
+
+// CUT:     READ_LastChild
+//const CAN_MOVE = R.propSatisfies(GET_childCount(frmSYM);  // maybe use R.isEmpty:: a->Bool
+const MOVE_LastChild = function MOVE_LastChild(frmGrp, toGrp) {
+    // eg toGrp.insert (frmGrp.last..)INFRONT_OF(toGrp.first..)e.g pst>cur; cur>fut
+    toGrp.insertBefore(R.prop('lastElementChild', frmGrp), R.prop('firsElementChild',toGrp));
+};
+// CUT:     READ_NextChild
+const MOVE_NextChild = function MOVE_NextChild(frmGrp, toGrp) {
+    // frm>to  e.g. fut>cur; cur>pst
+    toGrp.appendChild(frmGrp.firstElementChild);
+};
+// TESTS:   READ_Next
+C_Msg = "tst:READ_Last  \n";
+
+// TESTS:   READ_Next
+var G = V_Grp_NL;
+C_Msg = "tst:READ_Last  \n";
+C_Msg += '  EXP:curCount:' + G[CUR].childElementCount + '=== 1, \n';
+MOVE_NextChild(G[FUT], G[CUR]);
+C_Msg += '  EXP:curCount:' + G[CUR].childElementCount + '=== 2, \n';
+MOVE_NextChild(G[CUR], G[PST]);
+C_Msg += '  EXP:curCount:' + G[CUR].childElementCount + '=== 1, \n';
+C_Both(C_Msg);
+MOVE_NextChild(G[FUT], G[CUR]);
+MOVE_NextChild(G[CUR], G[PST]);
+MOVE_LastChild(G[CUR], G[FUT]);
+MOVE_LastChild(G[PST], G[CUR]);
+MOVE_LastChild(G[CUR], G[FUT]);
+MOVE_LastChild(G[PST], G[CUR]);
+
+//
+
 
 // SHORTEN TESTING W/O THESE
 SET_All_Verse_Styles (StyleObj);
