5b077bda6bcbea352bb0d09629cba11f22e5f482
clif edwards <clif.edwards.2@gmail.com>
2016-02-22 07:01:59 -0500

    WIP  MODIFIED testing TO USE define function; map(function, collection)
    BEGIN FP build up in testDoc.js
    PREPARED testDoc.js FOR just 1TAAT work on Styles
    STABLE: YES
    1TAAT: GET VerseStyles WORKING

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -8,17 +8,16 @@ var V_Grp_NL = GET_V_Grp_NL(book);
 // *** TESTS FOR Verse Styles
 // start at the end with FP
 // get one Verse grp NL - use FUT for now
-// map fn_setStyle(v,n,a)
 var V_FUT_Tmpl = '.ChptrReadGrps .cur  .VerseReadGrps > .fut div';
 var V_Fut_NL = [...book.querySelectorAll(V_FUT_Tmpl)];
-var tst = R.mapObjIndexed(
-    (v, n, a)=> {
-        return (`v.cls,n,a.len: ${v.className},${n},${a.length}`)
-            , V_Fut_NL
-    });
+// map fn_setStyle(v,n,a)
+var MOD_verse = function (v, n, a) {
+    TRACE_Both("v.cls,n,a.len: " + v.className + "," + n + "," + a.length), V_Fut_NL;
+};
+var verseArr = R.mapObjIndexed(MOD_verse, V_Fut_NL);
+//  TEST
 
 
 // *** TESTING just testDoc.html Events
 //SET_All_Verse_Styles(V_Grp_NL);
-tst;
 BindHandlers(book);
