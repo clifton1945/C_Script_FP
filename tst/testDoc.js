f64495ae3e464913c0470a1cb2b3322be6593858
clif edwards <clif.edwards.2@gmail.com>
2016-02-22 05:59:20 -0500

    WIP   BEGIN FP build up in testDoc.js PREPARED testDoc.js FOR just 1TAAT work on Styles STABLE: YES 1TAAT: GET VerseStyles WORKING

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -7,6 +7,11 @@ var V_Grp_NL = GET_V_Grp_NL(book);
 
 // *** TESTS FOR Verse Styles
 // start at the end with FP
+// get one Verse grp NL - use FUT for now
+// map fn_setStyle(v,n,a)
+var V_FUT_Tmpl = '.ChptrReadGrps .cur  .VerseReadGrps > .fut div';
+var V_Fut_NL =  book.querySelectorAll(V_FUT_Tmpl);
+
 
 // *** TESTING just testDoc.html Events
 //SET_All_Verse_Styles(V_Grp_NL);
