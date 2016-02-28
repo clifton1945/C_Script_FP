d1b45e49960cbf9fc940097e4c00f777f3d1aa72
clif edwards <clif.edwards.2@gmail.com>
2016-02-27 21:35:00 -0500

    REMOVED older TESTING the older set_verse_styles.js Code
    ONE_TAAT: BUILD UPDATE_thisNode USING FP ramda
    STABLE: YES
    
    ONE_TAAT: GET VerseStyles WORKING
        DONE 160227 ONE_TAAT: BUILD UPDATE_thisNode USING FP ramda

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -16,20 +16,6 @@ var C_Grp_NL = GET_C_Grp_NL(book);
 var V_Grp_NL = GET_V_Grp_NL(book);
 var Tst_DivFut_Vrs4 = V_Grp_NL.item(2).children.item(5);
 
-/**
- * TEST DATA FOR Verse Styles
- * */
-var V_FUT_Tmpl = '.ChptrReadGrps .cur  .VerseReadGrps > .fut div';
-var V_FUT_NL = book.querySelectorAll(V_FUT_Tmpl);
-var V_FUT_Ar = [...book.querySelectorAll(V_FUT_Tmpl)];
-//
-var V_PST_Tmpl = '.ChptrReadGrps .cur  .VerseReadGrps > .pst div';
-var V_PST_NL = book.querySelectorAll(V_PST_Tmpl);
-var V_PST_Ar = [...book.querySelectorAll(V_PST_Tmpl)];
-// *** TEST FUNCTIONS
-var fTRACE_Tmpl = (v, n, a)=>
-    `[v.style,n,a.len]: ${ v.style.fontSize}, ${n}, ${a.length}`;
-
 
 /**
  *    *** CODE FOR STYLING Verses
@@ -88,13 +74,17 @@ var RESTYLE_1_verse = function RESTYLE_1_verse(vNode, ndx, arr) {
 }; // -> UPDATED vNode.style
 //*** var MAP_
 //
-var VerseArr = R.mapObjIndexed(RESTYLE_1_verse, V_FUT_NL); // fn, NL->NL
-//  TEST
+//var VerseArr = R.mapObjIndexed(RESTYLE_1_verse, V_FUT_NL); // fn, NL->NL
+////  TEST
 
 
 /**
- * ********* TESTS *******************
+ *   ********* TESTS *******************
  * */
+// *** TEST FUNCTIONS
+var fTRACE_Tmpl = (v, n, a)=>
+    `[v.style,n,a.len]: ${ v.style.fontSize}, ${n}, ${a.length}`;
+
 var RET, EXP;
 
 
@@ -112,14 +102,11 @@ const CHANGE_VerseNodeStyle = function(prop, val, node) {
     return node
 };
 var tst_CHANGE_VerseNodeStyle = function (tst = false) {
-    // DID NOT USE lens
-    //var classLens = R.lensProp('class');
-    //var styleLens = R.lensProp('style');
-    //var colorLens = R.lensProp('color');
-    ////var futLens = R.lensProp('fut');
-    //var fontSizeLens = R.lensProp('fontSize');
-    //var so_ = (n) => n.style;
     if (tst) {
+        C_Both(
+`tst_CHANGE_VerseNodeStyle
+     EXP TO SEE changes in testDoc.html.`
+        );
         var VSO = Tst_DivFut_Vrs4.style;
         // CODE UNDER TEST
         //function CHANGE_VerseNodeStyle(prop, val, node) {
@@ -132,7 +119,7 @@ var tst_CHANGE_VerseNodeStyle = function (tst = false) {
         VSO.fontSize = "200%"; //FORCE fontSize
         VSO.color = "green"; //FORCE fontSize
         var b = R.prop('color', Tst_DivFut_Vrs4.style);
-        C_Both("style.before " + JSON.stringify(b));
+        C_Both("style.before IS " + JSON.stringify(b));
         // TEST  CODE UNDER TEST
 
         //CHANGE_VerseNodeStyle = function(prop, val, node);
@@ -141,12 +128,10 @@ var tst_CHANGE_VerseNodeStyle = function (tst = false) {
 
         // TEST AFTER
         var a = R.prop('color', Tst_DivFut_Vrs4.style);
-        C_Both("style.after " + JSON.stringify(a));
+        C_Both("style.after IS " + JSON.stringify(a));
         console.assert(a === 'red' && a !== b, "EXP VerseStyle color:'red' NOT ${a}")
     }
 };
-
-
 
 /**
  * :: collection -> Bool: t | f
@@ -162,7 +147,6 @@ const coll_len_gt_1 = function coll_len_gt_1(coll) {
     );
     return f(coll)
 };
-
 var tst_coll_len_gt_1 = function(tst = false) {
     if (tst) {
         C_Both('tst_coll_len_gt_1, EXP NO FAIL. ');
@@ -174,37 +158,9 @@ var tst_coll_len_gt_1 = function(tst = false) {
             , "FUT:true && PST:false");
     }
 };
+
+
 // ****************************
-//
-var Style_Wt = function StyleWt(nl) {
-    // thisStyleConstants: lrgWt, smlWt, calcCode OR thisStyleObj
-    // thisNode vars: node, nodeNdx, nodeList
-    //R.if(coll_len_gt_1(nl)
-    //    , ()=> "gt 1"
-    //    , ()=> "lte 1"
-    //)
-};
-
-/**
- * a FP_STYLE TEST: V_FUT_NL :: nodelist -> Str: targetNode.style.fontSize >>
- * style_fontSizefNode:: targetNode->fontSize>>
- * = (R.prop('fontSize', tNodeStyle))(styleObj) //-> fontSize of target Node
- *          (R.prop('style', tNode)(tNode)) -> the  styleObj of the targetNode
- * targetNode:: NL->lastNode
- * >> (NL)=> NL[R.prop('length', NL) - 1]
- */
-RET = ((NL) => {
-    return R.prop('fontSize'
-        , R.prop('style'
-            , NL[R.prop('length', NL) - 1]
-        ));
-})(V_FUT_NL);
-EXP = "50%";
-console.assert(RET === EXP
-    , `lastVerse.fontSize-EXP:${EXP}, NOT ${RET}'`
-);
-
-
 /**
  * *** TESTING just testDoc.html Events
  */
