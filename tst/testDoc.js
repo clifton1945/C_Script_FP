e62e2104c4078b20d65cc97a4bd14cb8f3738e97
clif edwards <clif.edwards.2@gmail.com>
2016-04-02 10:44:12 -0400

    WIP    getting better STABLE: true
    BRANCH: MUTATE_all
    
    WIP:   OneTATT:  FP SET_a_StyleObject
    
    OneTAAT:    CALC_n_SET_Style  !!!
    OneTAAT:    STYLE_eachVerse
    IN testDoc.js

--- tst/testDoc.js
+++ tst/testDoc.js
@@ -24,96 +24,6 @@ function main() {
 var book = GET_book();
 var MSG = '';
 
-
-/**
- *          tst_R_Lens 1st LEARNING Lens:
- *          similar if not the same as test in ramda_tests.js
- * @param tst
- * @returns {*}
- */
-var tst_R_Lens = function (tst = false) {
-    if (tst) {
-        MSG = 'tst_R_Lens/';
-        /**
-         *          Style Constants for testing: a  subset, IN this case 'fut' OF objects/StyleConstants
-         * @type {{2: {name: string, smlWt: number, lrgWt: number, calcWt: Function, styleTmpl: {backgroundColor: string, opacity: string, fontSize: string}}}}
-         */
-        var tstStyleConstants = {
-            2: {
-                name: 'fut'
-                , smlWt: .4
-                , lrgWt: .8
-                , calcWt (sObj, vObj) {
-                    //noinspection JSUnusedLocalSymbols
-                    let {ver, ndx, ary} = vObj;
-                    let {smlWt, lrgWt} = sObj;
-                    let len = ary.length - 1;
-                    return (len > 0)
-                        ? (-(lrgWt - smlWt) / len * ndx + lrgWt)
-                        : lrgWt;  // always lrgWt
-                }
-                , wt: 1
-                , aStyleObj: {
-                    backgroundColor: "rgba(145, 248, 29, 0.29)"
-                    , fontSize: '70%'
-                    , opacity: 0.5
-                }
-                , styleStr: `{"backgroundColor": "rgba(145, 248, 29, 0.29)", "opacity": "0.6", "fontSize": "75%"}`
-                //, styleTmpl: ` backgroundColor: "rgba(145, 248, 29, 0.29)", opacity: "${this.wt}", fontSize: "${this.wt}%"`
-            }
-        };
-//  ------------------ SET TEST ------------
-//  ------------------ INVOKE TEST ------------
-        MSG += '\n......Style lensPath W/ aStyleObj...........';
-        var Lens2SO_ = R.lensPath(['2', 'aStyleObj', 'fontSize']);
-        var SO1 = R.view(Lens2SO_, tstStyleConstants);
-        //MSG += '\n' + `  BEFORE: expect fontSize: ${JSON.stringify(SO1)}===70% `;
-        var SO2 = R.set(Lens2SO_, '25%', SO1);
-        var SO3 = R.view(Lens2SO_, SO2);
-        //MSG += '\n' + `  AFTER:  expect fontSize: ${JSON.stringify(SO3)}===25% `;
-
-        MSG += '\n USE Lens in DOM ->  ';
-        var aVerse_tmplt = '.book .ChptrReadGrps .cur  .VerseReadGrps > .fut div';
-        var aVerseNodeList = document.querySelectorAll(aVerse_tmplt);
-        //var EXPLORE R.invoker(1, 'querySelectorAll')
-
-        MSG += '\n' + `...USE headLens = R.lensIndex(0)`;
-        var headLens = R.lensIndex(0);
-        var lst5 = R.pipe(
-            R.prop('innerText')
-            , R.slice(16, 21)
-        )(
-            R.view(headLens, aVerseNodeList)
-        );
-        var exp = R.equals(lst5, 'ndx:2');
-        MSG += '\n' + `    expect ${lst5} === ndx:2 [${exp}]`;
-        //        TEST DATA
-        //NOTE: POSSIBLE REMOVED + of += REMOVED WHILE Focus Here
-        MSG += '\n\n' + "...USE a Style Lens TO MUTATE a Verse.style Property";
-        var theFirstVerse = aVerseNodeList.item(0);
-        //theFirstVerse.style.color = 'pink';
-        //        CODE UNDER TEST
-        /**
-         *  NOTE there IS NO theFirstVerse.style.color AT this point
-         */
-        var colorLens = R.lensPath(['style', 'color']);
-        MSG += '\n' + `BEFORE R.set color IS:[${R.view(colorLens, theFirstVerse)}]`;
-        // SETS the color property here.
-        var newStyle = R.set(colorLens, 'blue', theFirstVerse);
-        MSG += '\n' + ` AFTER R.set color IS:[${R.view(colorLens, theFirstVerse)}]
-     the DOM Verse WAS NOT MUTATED.`;
-        var ret = R.view(colorLens, newStyle);
-        MSG += '\n' + ` BUT R.set RETURNS a new Color Property: [${R.view(colorLens, newStyle)}]
-     which CAN BE USED TO ASSIGN TO theFirstVerse.style.color.`;
-
-        // APPLY this TO theVerse
-        theFirstVerse.style.color = ret;
-        C_Both(MSG);
-        var noop = true;
-
-    }
-};
-
 /**
  *   ------   TEST REQUIRED FUNCTIONS  -----
  * */
@@ -175,58 +85,6 @@ var tstCode = function (tst = false) {
                 TRACE_((obj) => `   IN  aStyleCssStr_:${JSON.stringify(obj)}`)
             );
         };
-        //
-        ///**
-        // *          aStyleCSS_FROM(template)(wt)-> CSS object
-        // * @param cssTmpl
-        // * @param wt
-        // * @returns {Function|*}
-        // */
-        //var aStyleCSS_ = R.curry(function (cssTmpl, wt) {
-        //    return cssTmpl;  // hard coded testing only
-        //});
-        //
-        ///**
-        // *              actually SET the Element's CSS style
-        // */
-        //var setStyle_FROM_styleCSS_ = R.curry(function (css, node) {
-        //    return setStyle(css, node)
-        //});
-        //
-        ///**
-        // *          aStyle_SET_FOR_aVerse_FROM_(styleCSS, node, ndx, coll) -> setStyle
-        // * :: function (o)(v,n,c)-> v
-        // * // Verse State IS
-        // * @param styleCnstnts
-        // * @param node
-        // * @param ndx
-        // * @param coll
-        // * @returns {*}  - node.style MUTATED
-        // * @constructor
-        // * @private
-        // */
-        //var aStyle_FOR_aVerse_ = function aStyleFORaVerse_(styleCnstnts, node, ndx, coll) {
-        //    // this is the callBack FOR R_forEach || map
-        //    // aStyleWt( styleCnstnts, ndx, coll)
-        //    // aStyleCssStr_(styleConstants)
-        //    // aStyleCSS_(styleCnstnts, aStyleWt)
-        //    // setStyle_FROM_styleCSS_(aStyleCSS_, node)
-        //    // 1. properly pipe wt -> css -> set in this
-        //
-        //    var aStyleCSS = aStyleCSS_(12345);
-        //    var setStyle_FROM_styleCSS = setStyle_FROM_styleCSS_();
-        //    var f, f_ = (styCnts) => R.pipe(
-        //        aStyleCssStr_
-        //        , TRACE(', #1')
-        //        , aStyleCSS  // hardcoded for test
-        //        , TRACE(', #2')
-        //        , setStyle_FROM_styleCSS
-        //        , TRACE(', #3')
-        //    );i
-        //    f = f_(styleCnstnts)(node);
-        //    return f()
-        //};
-        //var aStyle_FOR_aVerse_c_ = R.curry(aStyle_FOR_aVerse_);
 
         /**
          *          TEST_ONLY A subset, IN this case 'fut' OF objects/StyleConstants
@@ -284,9 +142,9 @@ var tstCode = function (tst = false) {
         MSG += '\n CALC_wt ->  ';
 
         SET_aVerse_Style_ = function SET_aVerse_Style_(elmnt, ndx, coll) {
+
             // (Str prop) => Obj Lens
-            var SET_a_fontSize_Lens_ = (prop) => R.lensPath(['style', prop]);
-            //var a_fontSizeLens = SET_a_fontSize_Lens_('fontSize'); // this is a lens_ obj
+            var SET_a_Sty_Lens_ = (prop) => R.lensPath(['style', prop]);
 
             // (Str: propStr) -> Str: formatted propStr
             var FORMAT_fontSize_ = n => `${n}%`;
@@ -303,15 +161,9 @@ var tstCode = function (tst = false) {
 
             var a_TEST_formatted_fontSize = SET_a_CssStyle_(51, 100);
 
-            var lens_ = SET_a_fontSize_Lens_('fontSize'); // partial
+            var lens_ = SET_a_Sty_Lens_('fontSize'); // partial
 
-            var SET_a_StyleObject_ = R.set( // (lens_)  (StyleObj: min, max) (element) ->
-                lens_
-                , SET_a_CssStyle_(51, 100)
-                , elmnt
-            );
-            //   :: Obj:lens_, StyleObj_ -> (elmnt) -
-            var VIEW_a_StyleObject_ = R.view(SET_a_fontSize_Lens_('fontSize'), SET_a_StyleObject_);
+            var STYLE_a_Verse = (propName, propCSS, elmnt ) => {return elmnt.style[propName] = propCSS.style.[propName]};
 
             /**
              *          THIS IS THE CALL BACK FUNCTION !!!!!!
@@ -324,16 +176,13 @@ var tstCode = function (tst = false) {
                 function STYLE_an_Element(styleStr, elmnt) {
                     // todo REFACT: MAKE this a ramda SET && VIEW.._a_StyleObject_
                     // as it is now This does not use the two functions
-                    var lens_ = SET_a_fontSize_Lens_(styleStr);
-                    var ret = R.view( //
-                        lens_
-                        , R.set( // (lens_)  (StyleObj: min, max) (element) ->
+                    var lens_ = SET_a_Sty_Lens_(styleStr);
+                    var ret = R.set( // (lens_)  (StyleObj: min, max) (element) ->
                             lens_
                             , SET_a_CssStyle_(51, 100)  // this is a test stub for style weight
                             , elmnt
-                        )
                     );
-                    return elmnt.style[styleStr] = ret
+                    return elmnt.style[styleStr] = ret[styleStr]
                 }
             );
             MSG += a_TEST_formatted_fontSize;
