02e3de68b48a95c5d889890ec736c804761f8da7
clif edwards <clif.edwards.2@gmail.com>
2016-04-07 14:25:44 -0400

    a_stylWt_MAKER()  IS WORKING::  PRODUCES AND C_Both's each weight. At least for th 7 verses in fut the function is  hardCODED  a_stylWt_MAKER()  W/O FP STABLE IS : true
    ADDED <script src=    ' a_stylWt_MAKER.js...>    TO testDoc.html
    a_stylWt_MAKER() now is the callBack function now being called in map a_styleWt_MAKER, all fut class elements.
    
    STATE IS: The testCode/if (tst) {...  PRODUCES a MSG  but want it to get closer and closer to the actual function returning
    
    WIP:   OneTATT:  FP SET_a_StyleObject
    BRANCH: MUTATE_all
    OneTAAT:    CALC_n_SET_Style  !!!
    OneTAAT:    STYLE_eachVerse
    IN testDoc.js

--- tst/a_stylWt_MAKER.js
+++ tst/a_stylWt_MAKER.js
@@ -6,21 +6,20 @@
 var a_stylWt_MAKER = R.curry(
     function a_stylWt_MAKER(stylDict, elem, ndx, coll) { //::-> aSty_Str
         // wt in the end should be
-        // => farWt + (nearWt - farWt) / vDenom * ndx  Where vDenom:: 1 || coll.length - 1
+        // => smlWt + (lrgWt - smlWt) / vDenom * ndx  Where vDenom:: 1 || coll.length - 1
         // NOTE: within a verse class collection of verses, the only var that changes in its Index
         let wt;
-        let smlWt = (R.pipe(R.prop('2'), R.prop('smlWt')))(stylDict);
-        let lrgWt = (R.pipe(R.prop('2'), R.prop('lrgWt')))(stylDict);
+        let smlWt = stylDict['2']['smlWt'];
+        let lrgWt = stylDict['2']['lrgWt'];
         // begin calc Wt
-        let vDenom = (R.pipe(
+        let vDenom = R.pipe(
             R.length
-            , R.unless(R.equals(1), R.dec) // DO NOT DECREMENT IF Len == 1
-            , R.divide(1)
-        ))(coll);
-        let sCoeff =  smlWt + (lrgWt - smlWt)/ vDenom ;
-        wt = sCoeff * ndx;
-        // rndm just for early testing
-        //var rndm = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+            , R.unless(R.equals(1), R.dec)// DO NOT DECREMENT IF Len == 1
+            )(coll);
+        let clasCoeff = function clasCoeff (n) {
+            return smlWt + n / vDenom * (lrgWt - smlWt)
+        };
+        wt = round2(clasCoeff(ndx));
         MSG += msg(wt); // TRACING ONLY
         return wt
     });
