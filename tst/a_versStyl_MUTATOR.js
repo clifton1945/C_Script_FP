5d73825d3425dc71950eac095efacf069234b1d9
clif edwards <clif.edwards.2@gmail.com>
2016-04-09 07:51:24 -0400

    ADDED  a TST_STYLE_CONST object INSIDE  a_versStyl_NUTATOR.js
    WIP:   OneTATT:  FP SET_a_StyleObject
    
    BRANCH: COMMON
    
    OneTAAT:    CALC_n_SET_Style  !!!
    OneTAAT:    STYLE_eachVerse
    IN testDoc.js

--- tst/a_versStyl_MUTATOR.js
+++ tst/a_versStyl_MUTATOR.js
@@ -3,6 +3,32 @@
  */
 'use strict';
 
+var the_versStyl_CONSTANTS = {
+    properties:{
+        opacity: "1"
+        , fontSize: "100%"
+
+    },
+    fut: {
+        name: 'fut'
+        , smlWt: .4
+        , lrgWt: .95
+        , backgroundColor: "rgba(255, 0, 0, 0.24)"
+    },
+    cur: {
+        name: 'cur',
+        smlWt: 1.0,
+        lrgWt: 1.0
+        //, backgroundColor: "rgba(255, 0, 0, 0.24)"
+    },
+    pst: {
+        name: 'pst'
+        , smlWt: 0.4
+        , lrgWt: 0.95
+        , backgroundColor: "rgba(145, 248, 29, 0.29)"
+    }
+};
+
 /**
  *              a_versStyl_MUTATOR::
  *      Object.
@@ -11,8 +37,8 @@
  */
 var a_versStyl_MUTATOR = R.curry(  // just always curry all my functions
     function a_versStyl_MUTATOR(stylCons, elem, ndx, coll) {
-        var elem_opacity = {opacity: ".5"};
-        var elem_fontSize = {fontSize: "50%"};
-        Object.assign(elem.style, elem_fontSize)
+        var elem_opacity = {opacity: ".75"};
+        var elem_fontSize = {fontSize: "75%"};
+        Object.assign(elem.style, elem_fontSize, elem_opacity)
     });
 
