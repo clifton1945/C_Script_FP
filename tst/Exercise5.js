a01a96db95e3f52e155666a7985160065e5a9ae4
clif edwards <clif.edwards.2@gmail.com>
2016-04-11 16:27:28 -0400

    STABLE: 'bonus 1: mine:>  LEARNING WITH https://drboolean.gitbooks.io/mostly-adequate-guide/content/ch5.html
    OneTAAT:    CALC_n_SET_Style  !!!
    OneTAAT:    STYLE_eachVerse
    IN testDoc.js

--- tst/Exercise5.js
+++ tst/Exercise5.js
@@ -2,7 +2,7 @@
  * Created by CLIF on 4/11/2016.
  */
 var _ = require('ramda');
-//var accounting = require('accounting');
+var accounting = require('accounting');
 
 // Example Data
 var CARS = [{
@@ -58,12 +58,12 @@ c_it(shout("send in the clowns"));
 // ============
 // Use _.compose() to rewrite the function below. Hint: _.prop() is curried.
 var isLastInStock = _.compose(_.prop('in_stock'), _.last);
-c_it(isLastInStock(CARS));
+c_it('#1 ' + isLastInStock(CARS));
 // Exercise 2:
 // ============
 // Use _.compose(), _.prop() and _.head() to retrieve the name of the first car.
 var nameOfFirstCar = _.compose(_.prop("name"), _.head);
-c_it(nameOfFirstCar(CARS));
+c_it('#2 ' + nameOfFirstCar(CARS));
 // Exercise 3:
 // ============
 // Use the helper function _average to refactor averageDollarValue as a composition.
@@ -77,18 +77,59 @@ var _average = function (xs) {
 //return _average(dollar_values);
 
 var dollar_values = _.map(_.prop('dollar_value'));
-var averageDollarValue = _.compose(_average, trace('after dollar_values'), dollar_values);
-c_it(averageDollarValue(CARS));
+var averageDollarValue = _.compose(
+    _average,
+    //trace('after dollar_values'),
+    dollar_values);
+c_it('#3 ' + averageDollarValue(CARS));
 
-// Exercise 4:
-// ============
-// Write a function: sanitizeNames() using compose that returns a list of lowercase and underscored car's names: e.g: sanitizeNames([{name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true}]) //=> ['ferrari_ff'].
+/**
+ *              Exercise 4:
+ *              ============
+ *Write a function: sanitizeNames() using compose that returns a list of lowercase and underscored car's names: e.g: sanitizeNames([{name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true}]) //=> ['ferrari_ff'].
+ */
 var _name = _.prop('name');
 var _lowerCase = _.toLower;
 var _underscore = _.replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
-
 var sanitizeNames = _.map(_.compose(
         _underscore, _lowerCase, _name
     )
 ); // map(_compose( _underscore, _toLower, _name))
-c_it(sanitizeNames(CARS));
+c_it('#4 ' + sanitizeNames(CARS));
+
+/**
+ *              Bonus 1:
+ * ============
+ * Refactor availablePrices with compose.
+ * @param cars
+ * @returns {string}
+ */
+var availablePrices = function (cars) {
+    var availablePrices = function (cars) {
+        var available_cars = _.filter(_.prop('in_stock'), cars);
+        return available_cars.map(function (x) {
+            return accounting.formatMoney(x.dollar_value);
+        }).join(', ');
+    };
+};
+c_it('Bonus 1: theirs:> ' + availablePrices(CARS));
+
+// NOW MY Code
+var _available_cars = _.filter(_.prop('in_stock'));
+
+var _dollars_formatted = function _dollars_formatted(x) {
+    return accounting.formatMoney(x)
+};
+var _dollar_value = _.prop('dollar_value');
+var _availableDollars = _.compose(
+    _dollars_formatted,
+    trace('after _dollar_value'),
+    _dollar_value
+);
+var _spacer = _.join(', ');
+var _availableCosts = _.compose(
+    _spacer,
+    _.map(_availableDollars),
+    _available_cars
+);
+c_it('bonus 1: mine:> ' + _availableCosts(CARS));
