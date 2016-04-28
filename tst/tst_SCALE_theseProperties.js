08235287bf3112de71029ef1f7c5cb2ba8a3a184
clif edwards <clif.edwards.2@gmail.com>
2016-04-28 18:11:59 -0400

    a newPlanned  decomposed step by step breakdown of styling

--- tst/tst_SCALE_theseProperties.js
+++ tst/tst_SCALE_theseProperties.js
@@ -14,10 +14,11 @@
  * Created by CLIF on 4/26/2016.
  *
  */
-
 "use strict";
+
 var R = require('ramda');
 import { C_It } from '..//src//modules-compiled';
+//var C_It = (txt) => {return console.log(txt)};
 var ret, _ret;
 /**
  *              _a_Wt:: NUM -> NUM
@@ -26,19 +27,7 @@ var ret, _ret;
  */
 var round2 = x => Math.round(x * 100) / 100;
 const StyleDict = {
-    property: {fontSize: "100%", opacity: 1.0, textAlign: "Center", backgroundColor: "rgba(145, 248, 29, 0.29)"},
-    scaleable: {
-        fontSize: function _fontSize(x) {
-            return x.toString() + '%'
-        },
-        opacity: function _opacity(x) {
-            return (x / 100).toString()
-        }
-    },
-    notscaleable: {
-        textAlign: "center"
-    }
-};
+    property: {fontSize: "100%", opacity: 1.0, textAlign: "Center", backgroundColor: "rgba(145, 248, 29, 0.29)"}};
 /**
  *          _a_scaledValue:: (NUM ndx) -> NUM scaleValue
  * @param i
@@ -52,7 +41,7 @@ var _a_scaledValue = function (i) {
  *          _a_frmtTmpl:: (DICT , STR propName) -> FN ( NUM: val) -> STR: val
  */
 var _a_frmtTmpl = R.curry(function _a_frmtTmpl(dict, propName) {
-    return dict.property[propName]
+    return dict[propName]
 });
 /**
  *          _a_frmtedValue:: (OBJ, STR -> (NUM) ->STR formatted str value
@@ -63,28 +52,48 @@ var _a_frmtTmpl = R.curry(function _a_frmtTmpl(dict, propName) {
  * @private
  */
 var _a_frmtedValue = R.curry(function (styledict, propName, ndx) {
-        return R.compose(_a_frmtTmpl(styledict)(propName), _a_scaledValue)(ndx)
+        return R.compose(_a_frmtTmpl(styledict)(propName), _a_scaledValue(ndx))
     });
-
-//var _a_keysLST = R.keys(R.prop("scaleable", R.prop("property")));
-//var a_keysLST = R.keys(R.prop("scaleable", R.prop("property", StyleDict)));
-
-//
-//var _opacityValu = function (val) {return R.assoc('opacity', _a_scaledValue(val))};
-//var _fontSizeValu = function (val) {return R.assoc('fontSize', val)};
-
+/**
+ *   /   A PLAN
+ *  _the_stylObj:: (sDict) => {obj}
+ *  _the_propVals::  {obj}=>[v]
+ *  _the_propKeys::  {obj}=>[k]
+ *  _MAP_
+ *  //          functions for different types of Property keys
+ *  //          TRIAGE BY: key OR valu numeric + string makeup
+ *  // mutate valu_n_str:: e.g. {fontSize: "25%" -> "35%"}
+     *  _oldValu:: (v) => alph + num + alph
+     *  _strRt:: _oldValu_num_n_alpha-> rt+alph
+     *  _this_propWt:: (ndx,[coll]) => wt
+     *  _this_propValu:: (wt, _strRt)=> "wt+alpha"
+ *  // mutate valu::       e.g. {opacity: "0.25" -> "0.35"}
+     *  _oldValu:: (v) => alph + num + alph
+     *  _old_propWt:: (ndx,[coll]) => wt
+     *  _this_propWt:: (wt, _func) => new wt
+     *  _this_propValu:: (wt)=> "wt"
+ *  // mutate str::        e.g. (testAlign: "center -> "left"}
+     *  _oldValu:: (v) => == alph
+     *  _this_propWt:: (ndx,[coll]) => wt  // for consistency: throw away
+     *  _this_propValu:: (new_prop)=>_thisPropValu
+ *
+ *  NOT SURE AT THIS POINT
+ *  ASSOC_(thisProp, _this_propValu)(_the_propObj)
+ *  _this_StylObj (ndx, coll) = {k:v}
+ *  _STYL_aVerse({thisStylObj, elem)=> elem
+ *
+ */
+//  TESTS
 var baseStyle = StyleDict.property;
-// MUTATE style: sequence: given ndx apply a_scaledValue(i) -> apply a_frmtValue(sName) ->
-// R.assoc:: Str - (a) - {k:v} -> {k:v}
+var fontSizeStyle = {fontSize:"70%"};
 var fi = i =>`${(i)}%`;
-var _CUT = i=> R.assoc("fontSize", fi, baseStyle); // WO(i)
-// TESTS
 var i = 10;
 var rng = R.range(0,7);
 var iLst = R.map(i=>i*10, rng);
-ret = R.map(_CUT, iLst);  // >> so far this returns 7 repeated base styles WITHOUT fontSize
-//C_It(ret);
-C_It(JSON.stringify(ret));
+//ret = R.map(_CUT, iLst);  // >> so far this returns 7 repeated base styles WITHOUT fontSize
+ret = _a_frmtedValue(StyleDict.scaleable, 'fontSize', i);
+C_It(ret);
+//C_It(JSON.stringify(ret));
 //console.assert(R.not(R.is(String, ret)), 'assert: CANNOT BE Str.');
 //console.assert(R.is(Object, ret), 'assert: MUST BE Obj.'); //  YEAH !!
 
