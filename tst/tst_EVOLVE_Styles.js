cc038a3c9b16cdb5b550ce39e2054cac6847c864
clif edwards <clif.edwards.2@gmail.com>
2016-04-29 15:31:18 -0400

    FINAL FORM  tst_EVOLVE_Styles.js    FP style getting better. BUT the _aWt function IS a Stub ONLY STABLE: true

--- tst/tst_EVOLVE_Styles.js
+++ tst/tst_EVOLVE_Styles.js
@@ -3,43 +3,45 @@ var R = require('ramda');
 import { C_It } from '..//src//modules-compiled';
 //var C_It = (txt) => {return console.log(txt)};
 var CUT, _CUT, ret, _ret;
+
 /**
- *              _a_Wt:: NUM -> NUM
+ *          StyleDict
+ * @type {{property: {fontSize: string, opacity: number, textAlign: string, backgroundColor: string}}}
+ */
+const StyleDict = {
+    property: {fontSize: "100%", opacity: 1.0, textAlign: "CENTER", backgroundColor: "rgba(145, 248, 29, 0.29)"}
+};
+var baseStyle = StyleDict.property;
+// CODE:
+/**
+ *              TEST STUB ONLY >> _a_Wt:: NUM -> NUM
  * @param i
  * @private
  */
 var _a_Wt = i => 35 + i * 10; // EXP ndx
-var round2 = x => Math.round(x * 100) / 100;
-const StyleDict = {
-    property: {fontSize: "100%", opacity: 1.0, textAlign: "CENTER", backgroundColor: "rgba(145, 248, 29, 0.29)"}
-};
-/**
- *          _newStyleObj
- * @type {StyleDict.property|{fontSize, opacity, textAlign, backgroundColor}}
- */
-
-//  TESTS
-var baseStyle = StyleDict.property;
 // CODE UNDER TEST
-var n = 6;
-var _appendPercent = (n) => `${n}%`;
-var _divid100 = (n) => R.divide(n, 100);//WORKS
-var _newStr = (s)=>R.always(s);
-// FINAL FORM
-//var _new_fontSize = (n) => R.always(_appendPercent(_a_Wt(n))); // WORKS
+var _appendPercent = (n) => `${n}%`;  // DO NOT UNDERSTAND HOW TO MAKE THIS Pointless ?
+//var _divide100 = (n) => R.divide(n, 100);//WORKS
+var _divide100 = R.flip(R.divide)(100);// WORKS
 var _new_fontSize = R.compose(R.always, _appendPercent, _a_Wt);
-//var _new_opacity = (n) => R.always(_divid100(_a_Wt(n))); //WORKS
-var _new_opacity = R.compose(R.always, _divid100, _a_Wt);
-var i = 3;
+var _new_opacity = R.compose(R.always, _divide100, _a_Wt);
+var _newStr = (s)=>R.always(s);
+
+var i = 7;
 var transformations = {
-    opacity:_new_opacity(i),
+    opacity:_new_opacity(i), // a function that can be applied to WHICH theKey OR theValue ?
     fontSize: _new_fontSize(i),
     textAlign: _newStr('right'),
 };
-ret = R.evolve(transformations, baseStyle);
+var new_stylObj = R.evolve(transformations, baseStyle);
+
+ret= new_stylObj;
 
 C_It(ret);
 C_It(JSON.stringify(ret));
+//var _new_fontSize = (n) => R.always(_appendPercent(_a_Wt(n))); // WORKS
+//var _new_opacity = (n) => R.always(_divide100(_a_Wt(n))); //WORKS
+// FINAL FORM
 
 
 
