b3bfd5b0406b6c6b969d5eae13913b1ac59fafc6
clif edwards <clif.edwards.2@gmail.com>
2016-04-30 14:01:27 -0400

    GOT IT WORKING RIGHT:  nice FP   _set_opacity_Wt(0 AND _set_fontSize_WT() IN           tst_EVOLE_Styles.js NEXT:     GO BACK TO  TO INCLUDE  EVOLVE_Styles IN simpleTests.js

--- tst/tst_EVOLVE_Styles.js
+++ tst/tst_EVOLVE_Styles.js
@@ -27,15 +27,42 @@ var _new_fontSize = R.compose(R.always, _appendPercent, _a_Wt);
 var _new_opacity = R.compose(R.always, _divide100, _a_Wt);
 var _newStr = (s)=>R.always(s);
 
-var i = 7;
-var transformations = {
-    opacity:_new_opacity(i), // a function that can be applied to WHICH theKey OR theValue ?
-    fontSize: _new_fontSize(i),
-    textAlign: _newStr('right'),
-};
-var new_stylObj = R.evolve(transformations, baseStyle);
+/**
+ *      WANT transformations TO RETURN Property objs ALREADY MUTATED BY a new ndx
+ *
+ * the transforms OBJECT as shown in ramda docs have Functions that can not be partialed
+ * they modify the values of a key. After the transform is invoked it is an object
+ *
+ *  Another way?? Will lens be better??
+ *
+ * @type {number}
+ */
+//
+//var i = 7;
+//var transformations = {
+//    opacity:_new_opacity(i), // a function that can be applied to WHICH theKey OR theValue ?
+//    fontSize: _new_fontSize(i),
+//    textAlign: _newStr('right'),
+//};
+//var new_stylObj = R.evolve(transformations, baseStyle);
 
-ret= new_stylObj;
+
+//var opacity_lens = R.lensProp('opacity');
+//ret = R.set(opacity_lens)(.1234)(baseStyle);// good> 0.1234
+//var _set_opac = R.set(opacity_lens);
+//ret = R.flip(_set_opac)(baseStyle)(.3456);//good> 0.3456
+//var _set_opacity = R.set(R.lensProp('opacity'));
+var _set_opacity = R.compose(R.set, R.lensProp)('opacity');
+//ret = _set_opacity(_a_Wt(3))(baseStyle);// good>>65
+var _set_opacity_Wt = R.compose(_set_opacity, _divide100, _a_Wt);
+ret = _set_opacity_Wt(2)(baseStyle);// good> 0.55
+
+var _set_fontSize = R.compose(R.set, R.lensProp)('fontSize');
+//ret = _set_fontSize(_a_Wt(3))(baseStyle);// good>>65
+var _set_fontSize_Wt = R.compose(_set_fontSize, _appendPercent, _a_Wt);
+ret = _set_fontSize_Wt(2)(ret);// good> fontSize: '0.55%' AND opacity: 0.55
+
+
 
 C_It(ret);
 C_It(JSON.stringify(ret));
