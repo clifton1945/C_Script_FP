594e862b67a8a8d01733b8bb26ddd09d57297938
clif edwards <clif.edwards.2@gmail.com>
2016-04-30 19:13:30 -0400

    BEGINNING TO CONVERT FROM  EVOLVE code INTO simpleTests.js  BUT since EVOLVE is not evolve any more I will change tst_EVOLVE_Style...TO tst_LensSet_Style

--- tst/simpleTests.js
+++ tst/simpleTests.js
@@ -25,23 +25,21 @@ var tstCode = function (tst = false) {
      * _qSelectAll :: String -> Node -> NodeList
      * Note: NodeList is array-like so you can run ramda list functions on it.
      */
+    var chptr_queryStr = '.book .ChptrReadGrps .cur';
     var fut_queryStr = '.book .ChptrReadGrps .cur  .VerseReadGrps > .fut .vers';
     var cur_queryStr = '.book .ChptrReadGrps .cur  .VerseReadGrps > .cur .vers';
     var pst_queryStr = '.book .ChptrReadGrps .cur  .VerseReadGrps > .pst .vers';
     var _a_clasNL = function _clasNL(divClasStr) {
         return _qSelectAll(divClasStr)(document)
     };
+
     MSG = 'MUTATE_aVersStyle > ';
     /**
      *           CODE UNDER TEST
      * @type {string}
      */
-    //WORKING Code IN functions,js
-
-    // NEW TEST CODE
-
     /**
- *      _STYLE_:: obj, node -> MUTATED node.style
+     *      _STYLE_:: obj, node -> MUTATED node.style
      *          curried
      * @param styleObj
      * @param node
@@ -50,23 +48,43 @@ var tstCode = function (tst = false) {
     var _STYLE_ = R.curry(function setStyle(styleObj, node) {
         return Object.assign(node['style'], styleObj);
     });
+    /**
+     *      _STYL_aVerse:: {OBJ}->(ELM e, NUM n, [a])-> ELM e
+     */
     var _STYL_aVerse = R.curry(function STYL_aVerse(versStylDict, elem, ndx, coll) {
-        // once inside this function, use ndx to WEIGHT some styles
-        var thisStylObj = _thisStylOBJ(versStylDict)(ndx, coll); // ndx applied to WEIGHT AND COMPOSE individual styles
+        var thisStylObj = R.compose(_set_opacity_Wt(ndx), _set_fontSize_Wt(ndx))(baseStyle); // ndx applied to WEIGHT AND COMPOSE individual styles
         _STYLE_(thisStylObj, elem);
 
         MSG += `..(i[${ndx}] ${elem.style.textAlign}, ${elem.style.fontSize}, ${elem.style.opacity})`;
     });
-
+    /**
+     *      _STYL_aVerseClass:: {OBJ}->(ELM e, NUM n, [a])-> ELM e
+     *
+     */
     var _STYL_aVerseClas = R.curry((sOBJ, arr) => R_forEachIndexed(_STYL_aVerse(sOBJ), arr));
-    // test composing stylOBJs
+    /**
+     *       _STYL_a_Chptr:: {OBJ}->(ELM: e, NUM: n, [a])-> ELM: a Chptr COLLECTION
+     *  a Book has three ChapterClasses [ClassOfChptrs]: .pst, .cur, .fut. Each is a Collection of 0->N Chapters AT any one time.
+     *  a ChapterClass [ClassOfChptrs]: is a Collection of 0->N Chapters AT any one time.
+     *  a Chapter has three VerseClasses [ClassOfVerses]: .pst, .cur, .fut. Each with a Collection of 0->N Verses AT any one time.
+     *  a VerseClass [ClassOfVerses] is a Collection of 0->N Verses AT any one time.
+     *  a Verse has 1-N Sentences, each with 1-N clauses, each with 0-N phrases,....
+     *
+     *  BUT ONLY the cur_ChptrClass IS READ. Though any or all Chapters may at any on time be in the cur_ChptrClass
+     */
+    var _STYL_aChptr = R.curry((sOBJ, arr) => R_forEachIndexed(_STYL_aVerseClas(sOBJ), arr));
 
     // test data
-    var tst_Dict = {};
+    const tst_Dict = {
+        chptr: {
+            fut: {name: 'fut'},
+            cur: {name: 'cur'},
+            pst: {name: 'fut'},
+        }
+    };
     var tst_NL = _a_clasNL(fut_queryStr);
     // test it
     _STYL_aVerseClas(tst_Dict, tst_NL);
-
     C_Both(MSG);
     var noop = '';
 };
