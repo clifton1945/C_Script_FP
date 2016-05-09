8b8d575e49c6c6a4690ffa7e910cc66eb4a0fe3e
clif edwards <clif.edwards.2@gmail.com>
2016-05-09 19:41:45 -0400

    SCRAP R.evolve  TO change all the CSSStyleObj in one shot   SOMEHOW do it one style property at a time

--- tst/tst_LensSet_Styles.js
+++ tst/tst_LensSet_Styles.js
@@ -29,25 +29,8 @@ var _eager_fontSize = R.compose(_appendPercent, _a_Wt_stub);// a -> b
 var _eager_opacity = R.compose(_divide100, _a_Wt_stub);
 var _new_Str = (s)=>R.always(s);
 
-//NOTE: NOW how update transformation functions WITH an index????
-//  these functions are applied TO the property object
-// here what happens now
-//      _new_fontSize(ndx) returns a FUNCTION: that returns the str || valu
-//      _newer_fontSize(ndx) returns a VALUE thus ng
-//  SO the two eager for ndx functions need to be satisfied BEFORE the .always
-// HEY FYI: .always -> a->(*-> a) i.e. a func  ; .identity:: a -> a i.e.
-
 // THERE IS another var transformers in simpleTests.js
-var transformers = (n)=> {    // (n) -> {}
-    return {// trans... REQUIRE a transformer FUNC
-        // the R.always returns a FUNC returning the satisfied _eager by n
-        fontSize: R.always(_eager_fontSize(n)), // must be a -> (*-> b)
-        opacity: R.always(_eager_opacity(n)),
-        textAlign: _new_Str('right')
-    }
-};
-// hey, ret is a NEW object
-ret = R.evolve(transformers(0), baseStyle);
+
 // ASSERT
 RET = ret;
 TST = R.equals('35%', R.prop('fontSize', RET));
