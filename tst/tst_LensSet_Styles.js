5317f766544f8584793ec9f5a79875d4dc06b031
clif edwards <clif.edwards.2@gmail.com>
2016-05-09 20:47:19 -0400

    WORKS   REFACT    R.compose( _fontSizeValu_eagerWt, _fontSizeWtObj_eagerNdx)  (index stub)  -> fontSize CssStyleOobj

--- tst/tst_LensSet_Styles.js
+++ tst/tst_LensSet_Styles.js
@@ -25,17 +25,24 @@ var _a_Wt_stub = i => 35 + i * 10; // (i)->EXP: 0<ndx<
 // CODE UNDER TEST
 var _appendPercent = (n) => `${n}%`;  // DO NOT UNDERSTAND HOW TO MAKE THIS Pointless ?
 var _divide100 = R.flip(R.divide)(100);// WORKS
-var _eager_fontSize = R.compose(_appendPercent, _a_Wt_stub);// a -> b
+var _fontSizeWtObj_eagerNdx = R.compose(_appendPercent, _a_Wt_stub);// a -> b
 var _eager_opacity = R.compose(_divide100, _a_Wt_stub);
 var _new_Str = (s)=>R.always(s);
 
-// THERE IS another var transformers in simpleTests.js
+// so how go to next step and mutate the/a fontSize Value
+var fontSize_Key = 'fontSize';
+var fontSize_Valu = function _fontSizeValu_eagerWt(propStr, cssSO, propValu) {
+    return R.assoc(propStr, propValu, cssSO);
+};
+var _fontSizeValu_eagerWt = fontSize_Valu('fontSize', baseStyle, R.__);
 
 // ASSERT
-RET = ret;
+_ret = R.compose(_fontSizeValu_eagerWt, _fontSizeWtObj_eagerNdx);
+RET = _ret(0);
 TST = R.equals('35%', R.prop('fontSize', RET));
 EXP = `'EXP: textAlign: '35%' NOT ${RET}`;
 console.assert(TST, EXP);
+ret = RET;
 C_It(ret);
 C_It(JSON.stringify(ret));
 
