42a5b0b26053201a60871bae3a25f37ffc048406
clif edwards <clif.edwards.2@gmail.com>
2016-05-24 06:51:27 -0400

    new WIP  SET_clss_elemS_01.js ADDED TO simpleTests.html

--- tst/_01_template.js
+++ tst/_01_template.js
@@ -2,33 +2,36 @@
  * _01_template.js
  * CHANGED by CLIF on 5/20
  */
+
 "use strict";
-var R = require('ramda');
+// var R = require('ramda');
 
 var C_It = function C_It(txt) {
     return console.log(txt);
 };
+var MSG, RET, EXP, CUT, _CUT, TST, noop = 0;
 /**
  *          HELPER FUNCTIONS
  */
-var MSG, RET, EXP, CUT, _CUT, TST, noop;
 
 
 /**
- *          CONFIRMATION OUTPUT & ASSERTS
+ *          TEST DATA
  */
-C_It(RET);
+var cur_chpt_elem =
+    /**
+     *          CODE UNDER TEST
+     */
+    /**
+     *          CONFIRMATION OUTPUT & ASSERTS
+     */
+    C_It(RET);
 C_It(JSON.stringify(RET));
 // ASSERT
 EXP = `'EXP: ${TST} NOT ${RET}`;
 TST = R.equals(TST, RET);
 console.assert(TST, EXP);
-// some more Index
-RET = Wt(0); //-> 50
-RET = Wt(L - 1); //-> 90
 
-noop = '';
-//};
-//main();
+noop = 1;
 
 
