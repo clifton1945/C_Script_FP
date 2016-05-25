c80b2d5636f553d0cceb785c06dcee6f1c4ba612
clif edwards <clif.edwards.2@gmail.com>
2016-05-25 19:29:13 -0400

    tst/SET_clss_elemS_02.js  WORKING  STABLE w/ no actual tests.  can start at an arbitrary target element AND both calc weight and set csd for the verse

--- tst/set_clss_elemS_02.js
+++ tst/set_clss_elemS_02.js
@@ -38,24 +38,34 @@ var slctr = '.book .ChptrReadGrps .cur  .VerseReadGrps .fut > .vers ';
 // NAMING THINGS
 var a_fut_trgt_elemS_sTUB = NodER(slctr);//->
 
-TRGT = a_fut_trgt_elemS_sTUB;
+TRGT = a_fut_trgt_elemS_sTUB;//-> verse 4
 CUT = R.prop('previousElementSibling')(TRGT);//-> null
-CUT = R.prop('nextElementSibling')(TRGT);//-> innerText: chptr:2 verse:5 ndx:4
+var TRGT_plusOne = R.prop('nextElementSibling')(TRGT);//-> innerText: chptr:2 verse:5 ndx:4
+
+// parent Name -> prop: parentNode
 var _parnt = R.prop('parentNode');//-> div.fut with  verse
 _CUT = _parnt(TRGT);
 RET = R.prop('className', _parnt(TRGT));
-C_It(RET);
-CUT = R.compose(R.prop('children'), R.prop('parentNode'))(TRGT);//-> HTMLCollection[6]
+// C_It(RET);
+
+// trgt siblings count  Maybe collections.length OR R.prop('children'), R.prop('parentNode'))(TRGT)
 var _fut_clss_trgt_elemS = R.compose(R.prop('children'), R.prop('parentNode'));//-> HTMLCollection[3]:: _fut_clss_trgt_elemS AKA cur_chpt_elem.children
 _CUT = _fut_clss_trgt_elemS;
-RET = _CUT(TRGT);
+CUT = _fut_clss_trgt_elemS(TRGT);//-> HTMLCollection[6]
+RET = CUT.length;//-> 6
 
-// a cur_chpt_elem div
-// var cur_chpt_elem = NodER(slctr);//-> div.VerseReadGrps
-// // a cur_chpt_elem HAS 3 children:: each a div w/ a name: pst, cur, fut
-// // each _fut_clss_trgt_elemS KNOWS its clss name. USE it In STYLING
-// var fut_clss_elem_sTUB = _fut_clss_trgt_elemS.item(2);//-> this div.fut ISa elem WITH a HTMLCollection::
+// trgt_index  IN parent children:: El:a -> Coll:[a] -> Num
+// R.indexOf:: a -> [a] -> Num
+RET = R.indexOf(TRGT)(CUT);//-> 0
+// change the target element 
+var TRGT_plusOne = R.prop('nextElementSibling')(TRGT);//-> innerText: chptr:2 verse:5 ndx:4
+RET = R.indexOf(TRGT_plusOne)(CUT);//-> 1
 
+// OK i HAVE enough To SEE it Works.
+// Can get trgt parent name. Used to retrieve style weight limits used in calc wt.numer;
+//                       AND Used to retrieve style csds
+// Can get parentNode.children.length. used in weight calc.denom
+// Can get trgt index in sibling collection. Used in weight calc.numer
 
 /**
  *          CONFIRMATION OUTPUT & ASSERTS
