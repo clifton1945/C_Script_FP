b7e28a4e89dbd7ed056b9f43894b4a9ae1607c4e
clif edwards <clif.edwards.2@gmail.com>
2016-06-02 18:15:47 -0400

    WIP  step through all class elements and MUTATE them - at least a stubs STABLE simpleTests:  with rClssCSD_init(rcE)    and rClss_Children(rcE)
    WIP -> get rClass className for each of the three rClasses

--- tst/simpleTests.js
+++ tst/simpleTests.js
@@ -35,31 +35,44 @@ const styleProps = {
 
 const myTap = R.tap(s=>C_Both(s));
 
-// // now apply new csd to an element.
-// var new_elem = document.querySelector('div #tst1');
-// Object.assign(new_elem.style, RET);// IT WORKS html altered!!
-// RESULTS
+// now apply new csd to an element.
+
+// a nodelist of rClasses: NL: pst, cur, fut
 var nl = cur_Chptr_rClss_NL;
+// test
 noop = 1;
 assert(nl[2].className, 'fut');
 assert(nl[2].childElementCount, 6);
 
+//
 const _rClssKey = (rcE) => R.prop('className')(rcE);//: E:e -> S:e.classNameKey
-const _get_my_cssStylDecl = R.curry((dct, key) => R.prop(key)(dct));// D:propDict -> S:key -> D:propDict
+const _get_my_cssStylDecl = R.flip(R.prop)(key)(dct);// D:propDict -> S:key -> D:propDict
 const _my_cssStylDecl = _get_my_cssStylDecl(styleProps);// S:key -> D:propDict
 const _rClssCSD_init = R.compose(_my_cssStylDecl, _rClssKey);//:(rcE)->D:this rClss's cssStylDecl
-var rClssCSD_init = R.map(_rClssCSD_init);//  L:nl -> [D:d, D:d, D:d]
+// test
+const rClssCSD_init = R.map(_rClssCSD_init);//  L:nl -> [D:d, D:d, D:d]
 RET = rClssCSD_init(nl);
 noop = 2;
 assert(RET[0].fontSize, "80%");
 
-
-const rClss_Elem_Children = elem => R.prop("children");// E:e -> L:[e, e,..]
-var new_elem = document.querySelector('div #tst1');
-RET = rClss_Elem_Children(new_elem);
+// the target list of verses to mutate
+const rClss_Elem_Children = R.prop("children");// E:e -> L:[e, e,..]
+// test
+var stub_rClss_Elem = document.querySelector('div #cur_VerseReadGrp');//
+RET = rClss_Elem_Children(stub_rClss_Elem);// -> HTMLCollection[2]
 noop = 3;
-// assert();
-// now set a rClss verses given, AS A TEST, the _init CSD AND elem
+assert(RET.length, 2);
+// now altr the style
+// var set = Object.assign(elem.style, dct(elem));//:(D:dct)(E:elem, N:ndx, L:col) ->  E.elem.style
+var CUT = R.forEach( //
+    eClss => { // uses _rClssCSD_init(eClss), rClssElem_Children(eClss)
+        R.forEach(
+            (trgt, ndx, col)=> Object.assign(trgt.style, _rClssCSD_init(eClss)),
+            rClss_Elem_Children(eClss)
+        )
+    },
+    stub_rClss_Elem
+);
 
 // _CUT = (dct, elem, ndx, col)=>{Object.assign(elem.style, dct(elem));};//:(D:dct)(E:elem, N:ndx, L:col) ->  E.elem.style
 // CUT = R.addIndex(R.map)(_CUT)();
