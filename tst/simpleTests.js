94239c3977907bde69b9a6fa52fb13c26a4c6572
clif edwards <clif.edwards.2@gmail.com>
2016-06-02 16:02:20 -0400

    STABLE simpleTests:  with rClssCSD_init(rcE)    and rClss_Children(rcE)  I have enough to step through all class elements and MUTATE them - at least a stubs WIP -> get rClass className for each of the three rClasses

--- tst/simpleTests.js
+++ tst/simpleTests.js
@@ -4,7 +4,7 @@
  * core functions for EVOLVER  are now in tst/tst_EVOLVER.js
  */
 
-var MSG = '', CUT, RET, EXP, TST, noop;
+var MSG = '', CUT, _CUT, RET, EXP, TST, noop = 0;
 
 /**
  *              TEST STUBS ONLY >>
@@ -16,8 +16,14 @@ const styleProps = {
     fut: {
         fontSize: "90%",
         opacity: 0.9,
-        textAlign: "CENTER",
+        textAlign: "left",
         backgroundColor: "rgba(145, 248, 29, 0.29)"
+    },
+    cur: {
+        fontSize: "100%",
+        opacity: 1.0,
+        textAlign: "center",
+        // backgroundColor: "rgba(255, 0, 0, 0.24)"
     },
     pst: {
         fontSize: "80%",
@@ -26,35 +32,37 @@ const styleProps = {
         backgroundColor: "rgba(255, 0, 0, 0.24)"
     }
 };
-// USING fontSize as test trgt: compose a final set_new_fontSize: S:val -> CSD:{k:v}
-// var fut_styleProps = R.flip(R.prop)(styleProps)('fut');
-// var fontSize_init = R.prop('fontSize')(fut_styleProps);
-// var fontSizer = R.replace(fontSize_init);
-// var fontSize_new = '333%'; // WEIGHTED AND FORMATTED. Though  could do without formatter
-//
-// /**
-//  *      transformers: -> O:{k:v,...}
-//  * @type {{fontSize, opacity: *}}
-//  */
-// var transformers = {// NOTE: USING .replace For string!
-//     fontSize: fontSizer(fontSize_new), // yeah, it works
-//     opacity: R.multiply(.5),
-//     // textAlign: R.replace('CENTER', 'right')// yeah, this works if uncommented
-// };
-// // EVOLVER
-// // let EVOLVER = R.evolve(transformers);
-// RET = EVOLVER(fut_styleProps);
+
+const myTap = R.tap(s=>C_Both(s));
 
 // // now apply new csd to an element.
 // var new_elem = document.querySelector('div #tst1');
 // Object.assign(new_elem.style, RET);// IT WORKS html altered!!
 // RESULTS
 var nl = cur_Chptr_rClss_NL;
+noop = 1;
 assert(nl[2].className, 'fut');
 assert(nl[2].childElementCount, 6);
 
-CUT = (elm, ndx, col)=> R.prop('className')(elm);//: (elm, ndx, col) -> S:elm.className
-RET = R.addIndex(R.map)(CUT)(nl);// -> [S,S,..]
-C_Both(JSON.stringify(RET));
-noop = 0;
+const _rClssKey = (rcE) => R.prop('className')(rcE);//: E:e -> S:e.classNameKey
+const _get_my_cssStylDecl = R.curry((dct, key) => R.prop(key)(dct));// D:propDict -> S:key -> D:propDict
+const _my_cssStylDecl = _get_my_cssStylDecl(styleProps);// S:key -> D:propDict
+const _rClssCSD_init = R.compose(_my_cssStylDecl, _rClssKey);//:(rcE)->D:this rClss's cssStylDecl
+var rClssCSD_init = R.map(_rClssCSD_init);//  L:nl -> [D:d, D:d, D:d]
+RET = rClssCSD_init(nl);
+noop = 2;
+assert(RET[0].fontSize, "80%");
 
+
+const rClss_Elem_Children = elem => R.prop("children");// E:e -> L:[e, e,..]
+var new_elem = document.querySelector('div #tst1');
+RET = rClss_Elem_Children(new_elem);
+noop = 3;
+// assert();
+// now set a rClss verses given, AS A TEST, the _init CSD AND elem
+
+// _CUT = (dct, elem, ndx, col)=>{Object.assign(elem.style, dct(elem));};//:(D:dct)(E:elem, N:ndx, L:col) ->  E.elem.style
+// CUT = R.addIndex(R.map)(_CUT)();
+RET = rClssCSD_init(nl);
+C_Both(JSON.stringify(RET));
+noop = 3;
