9843bb2e99be2d9628fd019d981d143370723ee0
clif edwards <clif.edwards.2@gmail.com>
2016-06-04 18:59:28 -0400

    WIP:  CANNOT USE import  CAN ADD <script src="tst/transformers_tests-compiled.js"></script>0   BEFORE   <script src="tst/simpleTests-compiled.js"></script>   IN simpleTests.html   AND  WIP / LEARN transformers stuff in its own file:  transformers_tests.js
    REFACT:  REVERSED parameters IN assert()  IN  functions.js AND simpleTests.js

--- tst/simpleTests.js
+++ tst/simpleTests.js
@@ -1,5 +1,18 @@
 "use strict";
+
+// import {transformers} from 'tst/transformers_tests-compiled';
+
 /**
+ * 6/4/16 @ 19:00
+ * WIP:
+ CANNOT USE import
+ CAN ADD <script src="tst/transformers_tests-compiled.js"></script>0
+ BEFORE   <script src="tst/simpleTests-compiled.js"></script>
+ IN simpleTests.html
+ AND  WIP / LEARN transformers stuff in its own file:  transformers_tests.js
+
+ REFACT:  REVERSED parameters IN assert()  IN  functions.js AND simpleTests.js
+
  * 6/4/2016 @ 07:02
  *      CLEANED UP code and doc for    stable _RESTYLE_all_trgts: L: nodelist -> L:nl
  *      all Elements in all rClasses RESTYLED =f(trgt)
@@ -68,11 +81,12 @@ const _base_clss_CSD = R.flip(R.prop)(CSD_D);
 // const _base_clss_CSD = R.compose(R.flip, R.prop)( CSD_D);// brokEN  S:key -> D:propDict
 
 /**
- *                  transformers:
+ *                  transformers: now located in transformers.js
+ * as REF R.replace:: RegExp|String -> String -> String -> String
  * @type {{fontSize, opacity: *, textAlign: (void|XML|string|*)}}
  */
 // let _stepER, _fontSizER;
-// _stepER = R.replace(50);//TODO clunky, this requires me to know the stepSize: default value NG
+// _stepER = R.replace(50);// Str -> Str -> Str
 // _fontSizER = R.replace('40%');//GIVEN: init==80% transformer Fn  S: valu ->
 // var transformers;
 // transformers = {// NOTE: USING .replace For string!
@@ -138,31 +152,31 @@ function TestMe() {
     CUT = _RESTYLE_all_trgts(nl); //INVOKEd
     trgt = document.querySelector('div #tst1');
     RET = trgt.style.stepSize;
-    assert(RET, 0, tNum);
+    assert(0, RET, tNum);
 //tests  _RESTYLE_trgts
     tNum = 4;
     CUT = _RESTYLE_all_trgts(nl); // INVOKED
     trgt = document.querySelector('div #tst1');
     RET = trgt.style.textAlign;
-    assert(RET, 'center', tNum);
+    assert('center', RET, tNum);
 // tests  _rClss_Elem_Children
     tNum = 3;
     let stub_rClss_Elem = document.querySelector('div #cur_VerseReadGrp');//
     RET = _rClss_Elem_Children(stub_rClss_Elem);// -> HTMLCollection[2]
-    assert(RET.length, 2, tNum);
+    assert(2, RET.length, tNum);
 
 //tests  _my_init_rClss_CSD
     tNum = 2;
     let _stub_my_init_rClss_CSD_List = R.map(_new_clss_CSD);//  L:nl -> [[D:d, D:d, D:d]]
     RET = _stub_my_init_rClss_CSD_List(nl);//  ->  [[D:d, D:d, D:d]]
-    assert(RET[0].fontSize, "60%", tNum);
-    assert(RET[2].fontSize, "90%", tNum);
+    assert("60%", RET[0].fontSize, tNum);
+    assert("90%", RET[2].fontSize, tNum);
 
 // test   cur_Chptr_rClss_NL
     tNum = 1;
     nl = cur_Chptr_rClss_NL;
-    assert(nl[2].className, 'fut', tNum);
-    assert(nl[2].childElementCount, 6, tNum);
+    assert('fut', nl[2].className, tNum);
+    assert(6, nl[2].childElementCount, tNum);
 // final MSG
     MSG = 'TestMe: completed';
     C_Both(MSG);
--- tst/transformers_tests.js
+++ tst/transformers_tests.js
@@ -8,14 +8,26 @@
 /**
  *                  transformers:
  * @type {{fontSize, opacity: *, textAlign: (void|XML|string|*)}}
+ *
+ * 
  */
-let _stepER, _fontSizER;
-_stepER = R.replace(50);//TODO clunky, this requires me to know the stepSize: default value NG
-_fontSizER = R.replace('40%');//GIVEN: init==80% transformer Fn  S: valu ->
+// let _stepER, _fontSizER;
+let _stepER = R.replace(50);
+/**
+ *          _fontSizER:: Str->Str->Str
+ * @type {void|XML|string|*}
+ * NOTE: using a broad regex like - /\d+/im - transforms All fontstyles
+ * NOTE: using a specific Str like - 40 - transforms in this case just the clss:cur
+ */
+// let _fontSizER = R.replace(/\d+/im);//any one or more digits, transformer Fn  S: valu ->
+let _fontSizER = R.replace('40');//any one or more digits, transformer Fn  S: valu ->
 
-var transformers = {// NOTE: USING .replace For string!
-    stepSize: R.multiply(1.5), //-> 75     
-    fontSize: _fontSizER('60%'), // GIVEN init == 40%-> 180%
-    opacity: R.multiply(2),
+let transformers = {// NOTE: USING .replace For string!
+    stepSize: R.multiply(1.5), //-> 75
+    fontSize: _fontSizER(200), // Str -> Str
+    opacity: R.multiply(3),
     textAlign: R.replace('right', 'center')// works FOR any rClss WITH initial 'center'
 };
+//
+//
+// _EVOLVE_clss_CSD = R.evolve(transformers); //  {k:v} ->{k:v}
