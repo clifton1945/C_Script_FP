7aeb2a5a564c12c7dd8490b0fb1fa4cdbdb5eab1
clif edwards <clif.edwards.2@gmail.com>
2016-06-06 16:00:05 -0400

    NEXT:  change steps to accept the class.childElementCount  directly from the class element. WIP - STABLE -  _step() AND clss_step() tested NEXT:  WIP transformers  functions for the style properties

--- tst/transformers_tests.js
+++ tst/transformers_tests.js
@@ -12,13 +12,14 @@ var RET, CUT, tstN = 0;
  * @type {{fontSize, opacity: *, textAlign: (void|XML|string|*)}}
  * 
  */
-// let _stepER, _fontSizER;
-// NOT USED YET   let _stepER = R.replace(/\d+/im);// N:new -> fn
 
-let _denom = R.compose(R.dec, R.length); // [L:a] -> N:n
+/**
+ *      :: [L:col] -> N:n
+ */
+let _denom = R.compose(R.dec, R.length); // [L:col] -> N:n
+let _denom1 = R.compose(R.dec); // N:n -> N:n
 tstN = 11;
-assert(-1, _denom([]), tstN);
-
+assert(-1, _denom(0), tstN);
 /**
  *      ::  L:col -> N:ndx -> 0 | N:n
  */
@@ -29,22 +30,23 @@ assert(0, _step([1])(111), tstN);// dec length < 1 -> 0
  *      ::  N:ndx -> 0 | N:n
  * USE: the Class childElementCount IS constant; set its slope
  */
-tstN = 10; // test: _linear
 var clss_step = _step([1, 2, 3]);
+tstN = 10; // test: _linear
 assert(0.0, clss_step(0), tstN);
 assert(0.5, clss_step(1), tstN);
 assert(1.0, clss_step(2), tstN);
 
-let stepSizER = (ndx, col)=> {
-};// thisClssCSD -> (elem, ndx, col) ->
 /**
  *          _fontSizER:: Str->Str->Str
  * @type {void|XML|string|*}
  * NOTE: using a broad regex like - /\d+/im - transforms All fontstyles
  * NOTE: using a specific Str like - 40 - transforms in this case just the clss:cur
  */
-// let _fontSizER = R.replace(/\d+/im);//any one or more digits, transformer Fn  S: valu ->
 let _fontSizER = R.replace('40');//any one or more digits, transformer Fn  S: valu ->
+let stepSizER = (ndx, col)=> {
+
+};// thisClssCSD -> (elem, ndx, col) ->
+let _stepSizER = R.replace(/\d+/im);//any one or more digits, transformer Fn  S: valu ->
 
 let transformers = {
     stepSize: R.multiply(1.5), //-> 75
@@ -52,5 +54,9 @@ let transformers = {
     opacity: R.multiply(3),
     textAlign: R.replace('right', 'center')// works FOR any rClss WITH initial 'center'
 };
+/**
+ *      :: CSD:{k:v) -> CSD{k:v}
+ *  evolves the base CSD BY weighting some of the  properties.
+ */
 let _EVOLVE_clss_CSD;
 _EVOLVE_clss_CSD = R.evolve(transformers); //  {k:v} ->{k:v}
