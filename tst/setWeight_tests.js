a7566499b7372d8b11132bcc40b57c0dfa0816ea
clif edwards <clif.edwards.2@gmail.com>
2016-06-07 15:03:15 -0400

    setWeights_tests.js   <<  CLEANUP

--- tst/setWeight_tests.js
+++ tst/setWeight_tests.js
@@ -5,7 +5,7 @@
 "use strict";
 
 /**
- *      STEP_ER:(N:sibl_Count)->(N:myNdx) -> N:step
+ *      ::(N:sibl_Count)->(N:myNdx) -> N:n  1<= wt factor >= 0
  */
 const _StepER = R.curry(
     /**
@@ -19,14 +19,22 @@ const _StepER = R.curry(
         var den = R.dec(N);
         return R.gte(den, 1) ? R.divide(ndx, den) : 0;
     });
+
+/**
+ *          :: (N:ndx -> N: <=1 wt factor >= 0
+ *    waiting for ndx to set the weight factor.
+ * @param sibN
+ * @private
+ */
 const _rClss_StepER = (sibN)=>_StepER(sibN, R.__);
+
 /**
  *          CONFIRMATION OUTPUT & ASSERTS
  */
 assert(0, _StepER(1)(200));// denominator < 1; always -> 0
 var this_StepER = _rClss_StepER(6);
 assert(0, this_StepER(0));//
-assert(0.4, this_StepER(2));//
+assert(0.6, this_StepER(3));//
 assert(1, this_StepER(5));// 5/(6-1)-> 1
 
 
